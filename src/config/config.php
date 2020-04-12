b17351aa1cf9ae0ab89c9d0377db1445b194fe0a
tkotosz <tkotosz@inviqa.com>
2020-04-12 10:50:18 +0200

    up

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,7 @@
 return [
     'app_dir' => '.fooapp',
     'root_requirements' => [
+        'tkotosz/fooapp-extension-api' => '*'
     ],
     'repositories' => [
         ['type' => 'path', 'url' => 'core-extensions/*'],
